d5220cd9951d79f821f11ada8c5ddfd9b69ca469
Ziang Li <ziangli@Ziangs-Air.lan>
2026-05-11 23:59:56 -0700

    Add handin file

--- pa2/student_kernel.py
+++ pa2/student_kernel.py
@@ -1,8 +1,9 @@
 KERNEL_CONFIGS = [
     {"BLOCK_M": 128, "BLOCK_N": 256, "BLOCK_K": 32, "num_warps": 8, "num_stages": 4},
-    {"BLOCK_M": 128, "BLOCK_N": 128, "BLOCK_K": 32, "num_warps": 4, "num_stages": 4},
-    {"BLOCK_M": 64, "BLOCK_N": 256, "BLOCK_K": 32, "num_warps": 8, "num_stages": 4},
     {"BLOCK_M": 256, "BLOCK_N": 128, "BLOCK_K": 32, "num_warps": 8, "num_stages": 3},
+    {"BLOCK_M": 128, "BLOCK_N": 256, "BLOCK_K": 32, "num_warps": 8, "num_stages": 4},
+    {"BLOCK_M": 128, "BLOCK_N": 128, "BLOCK_K": 32, "num_warps": 4, "num_stages": 4},
+    {"BLOCK_M": 64,  "BLOCK_N": 256, "BLOCK_K": 32, "num_warps": 8, "num_stages": 4},
 ]
 
 
